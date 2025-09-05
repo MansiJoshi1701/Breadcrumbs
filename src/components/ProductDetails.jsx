3cf7c580a9358c46ac1b78f1bb3cabf5fa737ecd
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-09-05 16:19:03 -0700

    Page added - ProductDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function ProductDetails() {
+
+  const { id } = useParams();
+  const [product , setProduct] = useState([]);
+
+  //1. Get all data from the source
+  useEffect(() => {
+
+    fetch(`https://dummyjson.com/products/${id}`)
+    .then(res => res.json())  
+    .then(res => setProduct(res))
+    
+  } , [id])
+  //2. Filter the data based on id
+  //3. Display details of filtered data
+
   return (
-    <div>ProductDetails</div>
+    <div>
+      <h1>Product Details</h1>
+      <div>
+        <img src={product.thumbnail} alt={id} />
+        <h3>{product.title}</h3>
+        <h3>${product.price}</h3>
+        <h3>{product.description}</h3>
+      </div>
+    </div>
+    
   )
 }
 
--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 
 function ProductListing() {
+
+  const [data , setData] = useState([]);
+
+  useEffect(() => {
+
+    fetch("https://dummyjson.com/products")
+    .then(res => res.json())
+    .then(res => setData(res.products));
+    
+  } , [])
+
   return (
-    <div>ProductListing</div>
+
+    <div>
+      <h1>Product Listing</h1>
+
+      <div className='product-grid'>
+        {
+          data.map(prod => {
+            return(
+              <div className='product-card' key={prod.id}>
+                
+                <Link to= {`/products/${prod.id}`}>
+                  <img src={prod.thumbnail} alt={prod.id}/>
+                  <h3>{prod.title}</h3>
+                  <h3>${prod.price}</h3>
+                </Link>
+
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+    
+
   )
 }
 
