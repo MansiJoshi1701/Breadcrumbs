2adf8f6328976d399e70b23b8355c675d161fa73
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-09-05 18:27:40 -0700

    Final commit

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -6,20 +6,30 @@ function Breadcrumbs() {
     //This hook has a prop 'pathname' : path of the URL (e.g., /products/item) so you can directly destructure
     //the returned location to get just the pathname 
     let { pathname } = useLocation(); 
-    pathname = pathname.split('/').filter(x => x);//filter is to remove empty space
+    pathname = pathname.split('/').filter(x => x);//filter is to remove empty space since pathname = [" " , "products" , "1"]
     let breadcrumbPath = "";
 
     return (
-        <div>
-            <Link to="/">Home</Link>
+        <div className='breadcrumbs'>
+            
+            {pathname.length > 0 && <Link to="/">Home</Link>}
             
                 {
                     pathname.map((name,index) => {
                         breadcrumbPath += `/${name}`;
 
-                        return(
-                            <span key={index}>/ <Link to={breadcrumbPath}>{name}</Link> </span>
-                        )
+                        //if last index, it shouldn't have hyperlink coz dont wanna navigate to the same page
+                        if(index === pathname.length -1){
+                            return(
+                                <span key={index}> /{name} </span>
+                            )
+                        }
+                        else{
+                            return(
+                                <span key={index}> <Link to={breadcrumbPath}>/{name}</Link> </span>
+                            )
+                        }
+                        
                     })
                 }
             
